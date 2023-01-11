7266270332fe42a863a30e658a6c9a386eadabae
yo <y.n_green@hotmail.co.jp>
2023-01-11 23:09:18 +0900

    done

---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "../images/無題.png";
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} />
+      <p>test@example.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
